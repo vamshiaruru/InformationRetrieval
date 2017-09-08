a4f666c1ffc6bfcadd8ebeb63c01fed380a67eae
vamshiaruru <vamshiaruru0@gmail.com>
2017-09-08 17:39:53 +0530

    added searching script. All that is left is to add a ranker using TF,IDF and Cosine rule

---
+++ search.py
@@ -0,0 +1,20 @@
+"""
+This is the script that is exposed to the GUI/user. It calls the ranking
+script, takes the query and searches for it.
+"""
+import shelve
+from contextlib import closing
+
+
+if __name__ == "__main__":
+    query = raw_input("Enter the query: ")
+    result_documents = {}
+    # using sets because convenient for set intersection
+    query_words = [word.lower().strip() for word in query.split()]
+    with closing(shelve.open("dictionary.db")) as dictionary:
+        query_posting_list = [dictionary.get(query_word, {}) for
+                              query_word in query_words]
+        result_documents = set.intersection(*query_posting_list)
+
+    # add ranker logic i.e calculate scores based of TF,IDF or Cosine.
+    print result_documents
